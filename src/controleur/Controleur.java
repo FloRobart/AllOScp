f5783c7577ed6b7d6ef75bc65832fe464d3af055
Floris <Floris.robart11@gmail.com>
2023-01-21 02:44:29 +0100

    creation des class de base

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,12 +1,21 @@
 package controleur;
 
+import ihm.FramePrincipale;
+import metier.Metier;
+
 
 public class Controleur
 {
+    private Metier          metier;
+    private FramePrincipale ihm;
+
+
     public Controleur()
     {
-        System.out.println("Ca marche");
+        this.metier = new Metier         ();
+        this.ihm    = new FramePrincipale();
     }
+
 
     public static void main(String[] args)
     {
---
+++ src/ihm/FramePrincipale.java
@@ -0,0 +1,17 @@
+package ihm;
+
+import javax.swing.JFrame;
+
+
+public class FramePrincipale extends JFrame
+{
+    public FramePrincipale()
+    {
+        this.setTitle("Frame principale");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+        
+        this.setVisible(true);
+    }
+}
