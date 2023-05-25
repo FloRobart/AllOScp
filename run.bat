f749a9fe2a00061555baa25bbcfa3afe8c6c9ff3
Floris Robart <florisrobart.pro@gmail.com>
2023-05-25 17:27:35 +0200

    modif du CLASSPATH

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ XCOPY "./donnees" "./bin/donnees" /E /Y >NUL
 call :genererCompileList ".\src"
 
 echo Compilation...
-call javac -cp "%CLASSPATH%;.\bin\donnees\jar_libraries\jdom-2.0.6.jar;." -encoding utf8 -d ".\bin" "@compile.list" && ( echo Lancement du programme... & call java -cp "%CLASSPATH%;.\bin;.\bin\donnees\jar_libraries\jdom-2.0.6.jar;." controleur.Controleur && echo Fin de l'execution. || ( echo. & echo Erreur d'EXECUTION. )) || echo Erreur de COMPILATION.
+call javac -cp "%CLASSPATH%;.\bin\donnees\jar_libraries\jdom-2.0.6.jar;" -encoding utf8 -d ".\bin" "@compile.list" && ( echo Lancement du programme... & call java -cp "%CLASSPATH%;.\bin;.\bin\donnees\jar_libraries\jdom-2.0.6.jar;" controleur.Controleur && echo Fin de l'execution. || ( echo. & echo Erreur d'EXECUTION. )) || echo Erreur de COMPILATION.
 
 goto :eof
 
